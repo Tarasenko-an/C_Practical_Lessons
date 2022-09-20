6a4f6b6331f376309f55e5e81d41c265285c05b7
Tarasenko-an <tarasenko-an@ngs.ru>
2022-09-20 21:34:08 +0700

    Lesson6

---
+++ Lesson6/Task001/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int SizeArray = 0;
+Console.WriteLine("Input size array: ");
+SizeArray = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input numbers: ");
+
+int[] array = new int[SizeArray];
+FillArray(array);
+ShowArray(array);
+Console.WriteLine($"Number of positive numbers in the array: {PositiveNum(array)}");
+
+//Вывод массива на экран
+void ShowArray(int[] arr)
+{
+    Console.Write($"Array: [{arr[0]}");
+    for (int i = 1; i < arr.Length; i++)
+    {
+        Console.Write($", {arr[i]}");
+    }
+    Console.Write("]\n");
+}
+
+//Заполнение массива с клавиатуры
+void FillArray(int[] arr)
+{
+    int length = arr.Length; 
+    int index = 0; 
+    while(index < length)
+    {
+        arr[index] = Convert.ToInt32(Console.ReadLine());
+        index++; 
+    }
+}
+
+//Подсчет количества четных чисел в массиве
+int PositiveNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ Lesson6/Task002/Program.cs
@@ -0,0 +1,37 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] func1 = new double[2];
+double[] func2 = new double[2];
+Console.WriteLine("Determination of the point of intersection of the lines given by the equations:/ny = k1 * x + b1/ny = k2 * x + b2");
+
+//Вводим значения функций
+InputValues(func1, "Enter variable values k1, b1 function: y = k1 * x + b1");
+InputValues(func2, "Enter variable values k1, b1 function: y = k2 * x + b2");
+
+//Вычисляем точку пересечения
+GetCrossing(func1, func2);
+
+//Метод ввода значений функции
+void InputValues(double[] arr, string mess)
+{
+    Console.WriteLine(mess);
+    int length = arr.Length; 
+    int index = 0; 
+    while(index < length)
+    {
+        arr[index] = Convert.ToInt32(Console.ReadLine());
+        index++; 
+    }
+}
+
+//Метод определения точки пересечения функций
+void GetCrossing(double[] func1, double[] func2)
+{   
+
+    double CoordinateX =  (func1[1]-func2[1])/(func2[0]-func1[0]);
+    double CoordinateY =  func1[0]*(func1[1]-func2[1])/(func2[0]-func1[0]) + func1[1];
+    Console.WriteLine($"Intersection point: [{CoordinateX}, {CoordinateY}]");
+}
+
